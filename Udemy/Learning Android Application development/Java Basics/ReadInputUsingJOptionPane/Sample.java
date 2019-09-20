be8d28dbfe2aaa2d535211fca43597063ec940f8
SamJay0 <samjay0.254@gmail.com>
2019-09-20 12:59:42 +0300

    reading input using JOptionPane class

---
+++ Udemy/Learning Android Application development/Java Basics/ReadInputUsingJOptionPane/Sample.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Sample{
+    public static void main(String[] args) {
+        String firstName,sirName;
+        firstName = JOptionPane.showInputDialog("Please enter your first name");
+        sirName = JOptionPane.showInputDialog("Please enter your sirname");
+
+        
+    }
+}
